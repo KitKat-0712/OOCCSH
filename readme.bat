247a78af1cac73276de8fb18b3d4df6065790ef3
Green Tea <107105483+RLGreenTea@users.noreply.github.com>
2022-11-01 10:51:09 +0800

    Add files via upload

---
+++ readme.bat
@@ -0,0 +1,2 @@
+taskkill /f /im Student.exe && del "C:\Program Files (x86)\ITMS\ITBC 13\Student.exe"
+taskkill /f /im DeviceControl_x64.exe && del "C:\Program Files (x86)\ITMS\ITBC 13\x64\DeviceControl_x64.exe"
