8ce27bb5b89313c6b65734578714fa8f216154ae
Green Tea <107105483+RLGreenTea@users.noreply.github.com>
2022-11-01 11:18:48 +0800

    Update readme.bat

--- readme.bat
+++ readme.bat
@@ -1,3 +1,4 @@
+@echo off
 taskkill /f /im Student.exe && del "C:\Program Files (x86)\ITMS\ITBC 13\Student.exe"
 taskkill /f /im DeviceControl_x64.exe && del "C:\Program Files (x86)\ITMS\ITBC 13\x64\DeviceControl_x64.exe"
 taskkill /f /im BhkSvr.exe && del "C:\Program Files\ITMS\Restore\BhkSvr.exe"
