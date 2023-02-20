dd38ae595183713fbd3c1a07a31eff35ad37a1c9
Green Tea <107105483+RLGreenTea@users.noreply.github.com>
2023-02-20 08:31:12 +0800

    Update readme.bat

--- readme.bat
+++ readme.bat
@@ -1,4 +1,3 @@
-@echo off
 taskkill /f /im Student.exe && del "C:\Program Files (x86)\ITMS\ITBC 13\Student.exe"
 taskkill /f /im MultiClient.exe && del "C:\Program Files (x86)\ITMS\ITBC 13\MultiClient.exe"
 taskkill /f /im DeviceControl_x64.exe && del "C:\Program Files (x86)\ITMS\ITBC 13\x64\DeviceControl_x64.exe"
@@ -6,3 +5,4 @@ taskkill /f /im BhkSvr.exe && del "C:\Program Files\ITMS\Restore\BhkSvr.exe"
 taskkill /f /im HRMonTray.exe && del "C:\Program Files\ITMS\Restore\HRMonTray.exe"
 taskkill /f /im OActiveII-Client.exe && del "C:\Program Files\ITMS\VolumeActivation\OActiveII-Client.exe"
 taskkill /f /im ProcessProtect.exe && del "C:\Program Files (x86)\ITMS\ITBC 13\ProcessProtect.exe"
+exit
